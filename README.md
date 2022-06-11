17438d5594a915e0d06ca84c2ae88fc3f5f56454
Marek Lukáš <tajnymag@gmail.com>
2022-06-11 15:55:34 +0200

    Initial v2.0 release

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # tinder-deblur
  Simple script using the official Tinder API to get clean photos of the users who liked you
+
+Simple script using the official Tinder API to get clean photos of the users who liked you
+
+## How to use
+
+1. install a userscript manager ([Violentmonkey](https://violentmonkey.github.io/), [Tampermonkey](https://www.tampermonkey.net/), [Greasemonkey](https://www.greasespot.net/), ...)
+2. install the script:
+    * either click [this link](https://raw.githubusercontent.com/tajnymag/tinder-deblur/main/tinder.user.js) and the manager will install the script for you
+    * or copy the URL below and install it manually through manager's UI
+    `https://raw.githubusercontent.com/tajnymag/tinder-deblur/main/tinder.user.js`
+3. browse the Tinder web app normally and the profiles that like you should be unblurred
+
+## How does it work
+
+Tinder API returns clean profile images on requests that have their headers stripped down. The fact that it has been working for at least 2 years now is actually quite unexpected. **Be prepared** for this exploit to be patched sooner or later.
---
+++ tinder.user.js
@@ -0,0 +1,62 @@
+// ==UserScript==
+// @name        Tinder Deblur
+// @namespace   Violentmonkey Scripts
+// @match       https://tinder.com/*
+// @grant       none
+// @version     2.0
+// @author      Tajnymag
+// @downloadURL https://raw.githubusercontent.com/tajnymag/tinder-deblur/main/tinder.user.js
+// @description Simple script using the official Tinder API to get clean photos of the users who liked you
+// ==/UserScript==
+
+async function unblur() {
+	const teasers = await fetch('https://api.gotinder.com/v2/fast-match/teasers', {
+		headers: { 'X-Auth-Token': localStorage.getItem('TinderWeb/APIToken') },
+	})
+		.then((res) => res.json())
+		.then((res) => res.data.results);
+	const teaserEls = document.querySelectorAll('.Expand.enterAnimationContainer > div:nth-child(1)');
+
+	for (let i = 0; i < teaserEls.length; ++i) {
+		const teaser = teasers[i];
+		const teaserEl = teaserEls[i];
+
+		const teaserImage = teaser.user.photos[0].url;
+
+		teaserEl.style.backgroundImage = `url(${teaserImage})`;
+	}
+}
+
+async function once(listener, event) {
+	return new Promise((resolve) => {
+		listener.addEventListener(event, () => {
+			listener.removeEventListener(event, resolve);
+			resolve();
+		});
+	});
+}
+
+async function main() {
+	// check if running as a userscript
+	if (typeof GM_info === 'undefined') {
+		console.warn(
+			'[TINDER DEBLUR]: The only supported way of running this script is through a userscript management browser addons like Violentmonkey, Tampermonkey or Greasemonkey!'
+		);
+		console.warn(
+			'[TINDER DEBLUR]: Script was not terminated, but you should really look into the correct way of running it.'
+		);
+	}
+
+	// wait for a full page load
+	await once(window, 'load');
+
+	// setup navigation observer
+	const appEl = document.querySelector('.App');
+	const observer = new MutationObserver(() => {
+		if (['/app/likes-you', '/app/gold-home'].includes(location.pathname)) {
+			unblur();
+		}
+	});
+	observer.observe(appEl, { subtree: true, childList: true });
+}
+main().catch(console.error);
