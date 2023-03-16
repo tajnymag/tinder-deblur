2dfcf821fe75c24b0ef43226d0cf52a1fe001ef0
Justus G <30385979+JustixDevelopment@users.noreply.github.com>
2023-03-16 18:58:18 +0100

    fixed indentation

--- tinder.user.js
+++ tinder.user.js
@@ -16,18 +16,18 @@ var photoIntervals = [];
  * Core logic of the script
  */
 async function unblur() {
-    const teasers = await fetchTeasers();
-    const teaserEls = document.querySelectorAll('.Expand.enterAnimationContainer > div:nth-child(1)');
+	const teasers = await fetchTeasers();
+	const teaserEls = document.querySelectorAll('.Expand.enterAnimationContainer > div:nth-child(1)');
 
-    for (let i = 0; i < teaserEls.length; ++i) {
-        const teaser = teasers[i];
-        const teaserEl = teaserEls[i];
-        const teaserImage = teaser.user.photos[0].url;
+	for (let i = 0; i < teaserEls.length; ++i) {
+		const teaser = teasers[i];
+		const teaserEl = teaserEls[i];
+		const teaserImage = teaser.user.photos[0].url;
 
-        if (teaserImage.includes('unknown')) continue;
+		if (teaserImage.includes('unknown')) continue;
 
-        if (teaserImage.includes('images-ssl')) {
-            const userId = teaserImage.slice(32, 56);
+		if (teaserImage.includes('images-ssl')) {
+			const userId = teaserImage.slice(32, 56);
 
 			if(cache.includes(userId))
 				continue;
@@ -103,15 +103,15 @@ async function unblur() {
  * @returns {Promise<any>}
  */
 async function fetchTeasers() {
-    return fetch('https://api.gotinder.com/v2/fast-match/teasers', {
-        headers: {
-            'X-Auth-Token': localStorage.getItem('TinderWeb/APIToken'),
-            platform: 'android',
-        },
-    })
-        .then((res) => res.json())
-        .then((res) => res.data.results)
-    	.catch(ignore => {});
+	return fetch('https://api.gotinder.com/v2/fast-match/teasers', {
+		headers: {
+			'X-Auth-Token': localStorage.getItem('TinderWeb/APIToken'),
+			platform: 'android',
+		},
+	})
+		.then((res) => res.json())
+		.then((res) => res.data.results)
+		.catch(ignore => {});
 }
 
 /**
@@ -120,15 +120,15 @@ async function fetchTeasers() {
  * @returns {Promise<any>}
  */
 async function fetchUser(id) {
-    return fetch(`https://api.gotinder.com/user/${id}`, {
-        headers: {
-            'X-Auth-Token': localStorage.getItem('TinderWeb/APIToken'),
-            platform: 'android',
-        },
-    })
-        .then((res) => res.json())
-        .then((res) => res.results)
-    	.catch(ignore => {});
+	return fetch(`https://api.gotinder.com/user/${id}`, {
+		headers: {
+			'X-Auth-Token': localStorage.getItem('TinderWeb/APIToken'),
+			platform: 'android',
+			},
+	})
+		.then((res) => res.json())
+		.then((res) => res.results)
+		.catch(ignore => {});
 }
 
 /**
@@ -138,12 +138,12 @@ async function fetchUser(id) {
  * @returns {Promise<void>}
  */
 async function once(target, eventType) {
-    return new Promise((resolve) => {
-        target.addEventListener(eventType, () => {
-            target.removeEventListener(eventType, resolve);
-            resolve();
-        });
-    });
+	return new Promise((resolve) => {
+		target.addEventListener(eventType, () => {
+			target.removeEventListener(eventType, resolve);
+			resolve();
+		});
+	});
 }
 
 /**
@@ -151,57 +151,57 @@ async function once(target, eventType) {
  * @returns {Promise<HTMLElement>}
  */
 async function waitForApp() {
-    const getAppEl = (parent) => parent.querySelector('.App');
-    let appEl = getAppEl(document.body);
+	const getAppEl = (parent) => parent.querySelector('.App');
+	let appEl = getAppEl(document.body);
 
-    if (appEl) return appEl;
+	if (appEl) return appEl;
 
-    return new Promise((resolve) => {
-        new MutationObserver((_, me) => {
-            appEl = getAppEl(document.body);
-			
-            if (appEl) {
-                me.disconnect();
-                resolve(appEl);
-            }
-        }).observe(document.body, { subtree: true, childList: true });
-    });
+	return new Promise((resolve) => {
+		new MutationObserver((_, me) => {
+			appEl = getAppEl(document.body);
+
+			if (appEl) {
+				me.disconnect();
+				resolve(appEl);
+			}
+		}).observe(document.body, { subtree: true, childList: true });
+	});
 }
 
 async function main() {
-    // check if running as a userscript
-    if (typeof GM_info === 'undefined') {
-        console.warn(
-            '[TINDER DEBLUR]: The only supported way of running this script is through a userscript management browser addons like Violentmonkey, Tampermonkey or Greasemonkey!'
-        );
-        console.warn(
-            '[TINDER DEBLUR]: Script was not terminated, but you should really look into the correct way of running it.'
-        );
-    }
+	// check if running as a userscript
+	if (typeof GM_info === 'undefined') {
+		console.warn(
+			'[TINDER DEBLUR]: The only supported way of running this script is through a userscript management browser addons like Violentmonkey, Tampermonkey or Greasemonkey!'
+		);
+		console.warn(
+			'[TINDER DEBLUR]: Script was not terminated, but you should really look into the correct way of running it.'
+		);
+	}
 
-    // wait for a full page load
-    await once(window, 'load');
-    const appEl = await waitForApp();
+	// wait for a full page load
+	await once(window, 'load');
+	const appEl = await waitForApp();
 
-    const pageCheckCallback = () => {
-        if (['/app/likes-you', '/app/gold-home'].includes(location.pathname)) {
-            console.debug('[TINDER DEBLUR]: Deblurring likes');
-            unblur();
-        } else {
+	const pageCheckCallback = () => {
+		if (['/app/likes-you', '/app/gold-home'].includes(location.pathname)) {
+			console.debug('[TINDER DEBLUR]: Deblurring likes');
+			unblur();
+		} else {
 			cache.splice(0, cache.length);
 			
 			for(var intervalId of photoIntervals) {
 				clearInterval(intervalId);
 			}
 		}
-    };
+	};
 
-    // setup navigation observer
-    const observer = new MutationObserver(pageCheckCallback);
-    observer.observe(appEl, { subtree: true, childList: true });
+	// setup navigation observer
+	const observer = new MutationObserver(pageCheckCallback);
+	observer.observe(appEl, { subtree: true, childList: true });
 
-    // setup loop based observer (every 5s)
-    setInterval(pageCheckCallback, 5_000);
+	// setup loop based observer (every 5s)
+	setInterval(pageCheckCallback, 5_000);
 }
 
 main().catch(console.error);
